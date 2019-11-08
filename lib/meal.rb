8f6925db963a2b5925b67d3afe90d1bcccaeefac
allison cortez <allizetorc@gmail.com>
2019-11-08 05:05:01 +0000

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,20 @@
+#setting up our Meal class as a 'joining' model between our Waiter and our Customer classes
 class Meal
+  
+  attr_accessor :waiter, :customer, :total, :tip 
+  
+  @@all = []
+  
+  def initialize(waiter, customer, total, tip=0)
+    @waiter = waiter 
+    @customer = customer 
+    @total = total 
+    @tip = tip 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end
 
 end
