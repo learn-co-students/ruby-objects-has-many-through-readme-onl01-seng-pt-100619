e9cc18aaa05169b1a0678e7fd9328d15fd2e878c
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2019-11-14 21:32:37 +0000

    Done.

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,38 @@
+require 'pry'
+
 class Customer
+  attr_accessor :name, :age
+ 
+  @@all = []
+ 
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+    # binding.pry
+  end
+ 
+  def self.all
+    @@all
+    # binding.pry
+  end
   
+ 
+  def new_meal(name, total, tip=0)
+    # binding.pry
+    Meal.new(name, self, total, tip)
+  end
+  
+  
+  def meals 
+    Meal.all.select {|meal| meal.customer == self}
+    # binding.pry
+  end
+  
+  def waiters
+    meals.collect(&:waiter)
+  end
+  
+  
+ 
 end
